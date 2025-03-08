03c903b51a8595c07c8b9d6061306ccb77a05632
Jack.lei <horatio.lei@gmail.com>
2025-03-08 17:52:46 +0800

    First

---
+++ homefinanceresources_backend/src/main/java/com/socky/homefinanceresources_backend/HomefinanceresourcesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.socky.homefinanceresources_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomefinanceresourcesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomefinanceresourcesBackendApplication.class, args);
+	}
+
+}
---
+++ homefinanceresources_backend/src/test/java/com/socky/homefinanceresources_backend/HomefinanceresourcesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socky.homefinanceresources_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomefinanceresourcesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
